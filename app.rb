8785adb71258bc4f9af30f03a91a0799df4d1e10
Nina Nakajima <99044060+yuzu-ginger@users.noreply.github.com>
2022-05-19 02:19:22 +0900

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'line/bot'
 require 'nasa_apod'
 require 'net/http'
 require 'json'
+require 'date'
 
 get '/' do
   'hello world!'
@@ -16,9 +17,13 @@ def client
     }
 end
 
-def nasa
+def nasa(date)
     api_key = ENV["NASA_API_KEY"]
-    uri = URI.parse("https://api.nasa.gov/planetary/apod?api_key=#{api_key}")
+    if date == 0
+        uri = URI.parse("https://api.nasa.gov/planetary/apod?api_key=#{api_key}")
+    else
+        uri = URI.parse("https://api.nasa.gov/planetary/apod?api_key=#{api_key}&date=#{date}")
+    end
     json = Net::HTTP.get(uri)
     data = JSON.parse(json)
     return data["url"], data["date"], data["title"]
@@ -41,31 +46,67 @@ post '/callback' do
             when Line::Bot::Event::MessageType::Text
                 if event.message['text'] == "今日の天文写真は？"
                     today = Date.today
-                    puts today
-                    url = nasa[0]
-                    date = nasa[1]
-                    title = nasa[2]
-                    if url =~ /jpg/
-                        space_image = {
-                            type: 'image',
-                            originalContentUrl: url,
-                            previewImageUrl: url
-                        }
-                    else
-                        space_image = {
-                            type: 'text',
-                            text: url
-                        }
-                    end
-                    message = {
-                        type: 'text',
-                        text: "#{date}\n#{title}"
-                    }
-                    client.reply_message(event['replyToken'], [space_image, message])
+                    data = nasa(0)
+                    url = data[0]
+                    date = data[1]
+                    title = data[2]
+                    reply_image(event, url, date, title)
+                elsif event.message['text'] == "あの日の天文写真は？"
+                    client.reply_message(event['replyToken'], select_date(today))
                 end
             end
+        when Line::Bot::Event::Postback
+            puts "ok"
+            user_date = event['postback']['params']['date']
+            data = nasa(user_date)
+            url = data[0]
+            date = data[1]
+            title = data[2]
+            reply_image(event, url, date, title)
         end
     end
   
     "OK"
+end
+
+def select_date(today)
+    {
+    "type": "template",
+        "altText": "this is a buttons template",
+        "template": {
+            "type": "buttons",
+            "title": "Please select a date",
+            "text": "いつの天文写真を見ますか？",
+            "actions": [
+                {
+                  "type": "datetimepicker",
+                  "label": "select date",
+                  "mode": "date",
+                  "data": "action=datetemp&selectId=1",
+                  "max": today,
+                  "min": "1995-06-20"
+                }
+            ]
+        }
+    }
+end
+
+def reply_image(event, url, date, title)
+    if url =~ /jpg/
+        space_image = {
+            type: 'image',
+            originalContentUrl: url,
+            previewImageUrl: url
+        }
+    else
+        space_image = {
+            type: 'text',
+            text: url
+        }
+    end
+    message = {
+        type: 'text',
+        text: "#{date}\n#{title}"
+    }
+    client.reply_message(event['replyToken'], [space_image, message])
 end
